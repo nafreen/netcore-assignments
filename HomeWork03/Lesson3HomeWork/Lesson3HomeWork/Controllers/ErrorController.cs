7d5d934dc7caf854255c945c583c29041d7da6fa
Nashid Afreen <nashid77@yahoo.com>
2018-11-04 10:49:11 -0600

    HomeWork3 Added

---
+++ HomeWork03/Lesson3HomeWork/Lesson3HomeWork/Controllers/ErrorController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson3HomeWork.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult Index(int? statusCode)
+        {
+            switch (statusCode)
+            {
+                case 404:
+                    return View("Error404");
+                case 500:
+                    return View("Error500");
+                default:
+                    return View();
+            }
+            
+        }
+    }
+}
---
+++ HomeWork03/Lesson3HomeWork/Lesson3HomeWork/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lesson3HomeWork.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
